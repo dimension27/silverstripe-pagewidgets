a315ca1b9dcb3e91fe382ee2cd321075a71c416f
Simon Wade <simon.wade@gmail.com>
2012-10-04 21:51:55 +1000

    Add support for a Link on ImageWidget

--- code/widgets/ImageWidget.php
+++ code/widgets/ImageWidget.php
@@ -10,11 +10,21 @@ class ImageWidget extends PageWidget {
 
 	static $singular_name = 'Image Widget';
 
+	static $extensions = array('LinkFieldsDecorator');
+
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
 		$fields->addFieldToTab('Root.Image', $field = new FileUploadField('Image'));
 		PageWidget::set_upload_folder($field);
+		LinkFields::addLinkFields($fields, null, 'Root.Link');
 		return $fields;
 	}
 
+	public function SizedImage( $width, $height ) {
+		$rv = parent::SizedImage($width, $height);
+		if( $this->LinkURL() ) {
+			$rv = $this->Anchor($rv->forTemplate());
+		}
+		return $rv;
+	}
 }
